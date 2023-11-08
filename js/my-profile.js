7495090657fc10a45bbf20e5600a4e4c25484e73
beaglecoding <matiasmartindossantos@gmail.com>
2023-11-08 00:17:49 -0300

    validacion de formulariooo
    falta la carga de la foto

--- js/my-profile.js
+++ js/my-profile.js
@@ -43,22 +43,241 @@ document.addEventListener("DOMContentLoaded", () => {
     ctelefono.value = usuarioActivo.telefonos[0];
     profileimg.src = usuarioActivo.imagen_perfil.image_source[usuarioActivo.imagen_perfil.selected];
     datadisplay.forEach(input => {
-            input.disabled = true;
+        input.disabled = true;
     });
+
+
+
+    //LOGICA PARA BOTON DE MODIFICAR   
+
 
     btneditinfo.addEventListener("click", () => {
         changeDataState(datadisplay);
         pnombre.value = usuarioActivo.nombre;
-        snombre.vaule = usuarioActivo.snombre;
+        snombre.value = usuarioActivo.snombre;
         papellido.value = usuarioActivo.apellido;
         sapellido.value = usuarioActivo.sapellido;
         cemail.value = usuarioActivo.email;
         ctelefono.value = usuarioActivo.telefonos[0];
         savechanges.classList.remove("oculto");
+        btneditinfo.innerHTML = "";
+
+
+        if (pnombre.value.length > 0) {
+            usuarioActivo.nombre = pnombre.value;
+            pnombre.classList.remove("is-invalid");
+            pnombre.classList.add("is-valid");
+            alerterror1userform.innerHTML = "";
+            localStorage.setItem("Usuariosdb", JSON.stringify(baseDatos));
+        } else {
+            pnombre.classList.remove("is-valid");
+            pnombre.classList.add("is-invalid");
+            alerterror1userform.classList.add("invalid-feedback");
+            alerterror1userform.innerHTML = "Debe de ingresar un valor valido";
+        }
+
+
+
+        if (papellido.value.length > 0) {
+            usuarioActivo.apellido = papellido.value;
+            papellido.classList.remove("is-invalid");
+            papellido.classList.add("is-valid");
+            alerterror3userform.innerHTML = "";
+            localStorage.setItem("Usuariosdb", JSON.stringify(baseDatos));
+        } else {
+            papellido.classList.remove("is-valid");
+            papellido.classList.add("is-invalid");
+            alerterror3userform.classList.add("invalid-feedback");
+            alerterror3userform.innerHTML = "Debe de ingresar un valor valido";
+
+        }
+
+
+
+        usuarioActivo.sapellido = sapellido.value;
+        sapellido.classList.remove("is-invalid");
+        sapellido.classList.add("is-valid");
+
+
+
+        usuarioActivo.snombre = snombre.value;
+        snombre.classList.remove("is-invalid");
+        snombre.classList.add("is-valid");
+
+
+
+        if (cemail.value.includes("@")) {
+            usuarioActivo.email = cemail.value;
+            cemail.classList.remove("is-invalid");
+            cemail.classList.add("is-valid");
+            alerterror4userform.innerHTML = "";
+
+        } else {
+            cemail.classList.remove("is-valid");
+            cemail.classList.add("is-invalid");
+            alerterror4userform.classList.add("invalid-feedback");
+            alerterror4userform.innerHTML = "Debe ser un correo valido";
+
+        }
+
+
+        if (ctelefono.value.length > 6) {
+            usuarioActivo.telefonos[0] = ctelefono.value;
+            ctelefono.classList.remove("is-invalid");
+            ctelefono.classList.add("is-valid");
+            alerterror2userform.innerHTML = "";
+
+        } else {
+            ctelefono.classList.remove("is-valid");
+            ctelefono.classList.add("is-invalid");
+            alerterror2userform.classList.add("invalid-feedback");
+            alerterror2userform.innerHTML = "Debe contar con almenos 6 digitos";
+        }
+
+
     })
 
+
+    //LOGICA PARA BOTON DE GUARDAR   
+
+
+
     savechanges.addEventListener("click", () => {
-        changeDataState(datadisplay);
+        if (pnombre.classList.contains("is-invalid") || papellido.classList.contains("is-invalid") || sapellido.classList.contains("is-invalid") || snombre.classList.contains("is-invalid") || cemail.classList.contains("is-invalid") || ctelefono.classList.contains("is-invalid"))
+        {
+            alert("NO SE PUEDE GUARDAR EL USUARIO SIN DATOS CORRECTOS")
+
+
+         }
+         else{
+
+
+
+
+
+            changeDataState(datadisplay);
         savechanges.classList.add("oculto");
+        const saveChanges = document.getElementById("save-userchanges");
+
+        pnombre.classList.remove("is-valid");
+        papellido.classList.remove("is-valid");
+        sapellido.classList.remove("is-valid");
+        snombre.classList.remove("is-valid");
+        cemail.classList.remove("is-valid");
+        ctelefono.classList.remove("is-valid");
+
+        btneditinfo.innerHTML = "Editar datos de usuario";
+    }
+
     })
+
+
+
+
+    //LOGICA PARA GUARDAR LOS DATOS EN EL FORMULARIO Y VALIDARLO 
+
+
+    const alerterror1userform = document.getElementById("alerterror1userform");
+    const alerterror2userform = document.getElementById("alerterror2userform");
+    const alerterror3userform = document.getElementById("alerterror3userform");
+    const alerterror4userform = document.getElementById("alerterror4userform");
+
+
+
+
+
+    function actimputuser() {
+
+        pnombre.addEventListener("input", function () {
+            if (pnombre.value.length > 0) {
+                usuarioActivo.nombre = pnombre.value;
+                pnombre.classList.remove("is-invalid");
+                pnombre.classList.add("is-valid");
+                alerterror1userform.innerHTML = "";
+                localStorage.setItem("Usuariosdb", JSON.stringify(baseDatos));
+            } else {
+                pnombre.classList.remove("is-valid");
+                pnombre.classList.add("is-invalid");
+                alerterror1userform.classList.add("invalid-feedback");
+                alerterror1userform.innerHTML = "Debe de ingresar un valor valido";
+            }
+        })
+
+
+
+        papellido.addEventListener("input", function () {
+            if (papellido.value.length > 0) {
+                usuarioActivo.apellido = papellido.value;
+                papellido.classList.remove("is-invalid");
+                papellido.classList.add("is-valid");
+                alerterror3userform.innerHTML = "";
+                localStorage.setItem("Usuariosdb", JSON.stringify(baseDatos));
+            } else {
+                papellido.classList.remove("is-valid");
+                papellido.classList.add("is-invalid");
+                alerterror3userform.classList.add("invalid-feedback");
+                alerterror3userform.innerHTML = "Debe de ingresar un valor valido";
+
+            }
+
+        })
+
+        sapellido.addEventListener("input", function () {
+
+            usuarioActivo.sapellido = sapellido.value;
+            sapellido.classList.remove("is-invalid");
+            localStorage.setItem("Usuariosdb", JSON.stringify(baseDatos));
+            sapellido.classList.remove("is-invalid")
+            sapellido.classList.add("is-valid");
+
+
+        })
+
+
+        snombre.addEventListener("input", function () {
+
+            usuarioActivo.snombre = snombre.value;
+            snombre.classList.remove("is-invalid");
+            snombre.classList.add("is-valid");
+            localStorage.setItem("Usuariosdb", JSON.stringify(baseDatos));
+
+        })
+
+        cemail.addEventListener("input", function () {
+
+            if (cemail.value.includes("@")) {
+                usuarioActivo.email = cemail.value;
+                cemail.classList.remove("is-invalid");
+                cemail.classList.add("is-valid");
+                alerterror4userform.innerHTML = "";
+                localStorage.setItem("Usuariosdb", JSON.stringify(baseDatos));
+            } else {
+                cemail.classList.remove("is-valid");
+                cemail.classList.add("is-invalid");
+                alerterror4userform.classList.add("invalid-feedback");
+                alerterror4userform.innerHTML = "Debe ser un correo valido";
+
+            }
+        })
+
+        ctelefono.addEventListener("input", function () {
+            if (ctelefono.value.length > 6) {
+                usuarioActivo.telefonos[0] = ctelefono.value;
+                ctelefono.classList.remove("is-invalid");
+                ctelefono.classList.add("is-valid");
+                alerterror2userform.innerHTML = "";
+                localStorage.setItem("Usuariosdb", JSON.stringify(baseDatos));
+            } else {
+                ctelefono.classList.remove("is-valid");
+                ctelefono.classList.add("is-invalid");
+                alerterror2userform.classList.add("invalid-feedback");
+                alerterror2userform.innerHTML = "Debe contar con almenos 6 digitos";
+            }
+        })
+    }
+
+
+    actimputuser();
+
 });
+
